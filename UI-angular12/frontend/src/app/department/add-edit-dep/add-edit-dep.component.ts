0fd3b458f7f5a1b6e728dd3249956b9f24041110
architajoshi1970@gmail.com <architajoshi1970@gmail.com>
2021-09-02 08:36:01 +0530

    First Commit

---
+++ UI-angular12/frontend/src/app/department/add-edit-dep/add-edit-dep.component.ts
@@ -0,0 +1,39 @@
+import { asLiteral } from '@angular/compiler/src/render3/view/util';
+import { Component, OnInit, Input} from '@angular/core'; //Add this
+import { SharedService } from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-add-edit-dep',
+  templateUrl: './add-edit-dep.component.html',
+  styleUrls: ['./add-edit-dep.component.css']
+})
+export class AddEditDepComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  @Input() dep:any ="";  //Add this
+  DepartmentId:string;
+  DepartmentName:string;
+
+  ngOnInit(): void {
+    this.DepartmentId = this.dep.DepartmentId;
+    this.DepartmentName = this.dep.DepartmentName;
+  }
+
+  addDepartment(){
+    var val = {DepartmentId:this.DepartmentId,
+               DepartmentName:this.DepartmentName};
+    this.service.addDepartment(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
+
+  updateDepartment(){
+    var val = {DepartmentId:this.DepartmentId,
+      DepartmentName:this.DepartmentName};
+    this.service.updateDepartment(val).subscribe(res=>{
+    alert(res.toString());
+    });
+  }
+
+}
